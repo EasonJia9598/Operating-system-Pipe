be3bbeb936b81c85e383dd4ba2373fa16ee8d75b
Eason Jia <will.jia.sheng@gmail.com>
2018-09-23 20:28:13 -0300

    Initial Commit

--- Pipe/main.cpp
+++ Pipe/main.cpp
@@ -6,10 +6,44 @@
 //  Copyright © 2018 WillJia. All rights reserved.
 //
 
-#include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
+
+char string1[] = "Hello from parent";
+char string2[] = "Hi from child";
+
+int main()
+{
+    char buf[1024];
+    int i, fds1[2], fds2[2];
+    pipe(fds1);
+    pipe(fds2);
+    
+    if (fork()>0){
+        //Parent process starts
+        for (i=0; i<3; i++) {
+            //write parent message into fds1 close(fds1[0]);
+            write(fds1[1], string1,sizeof(string1));
+            //read child message from fds2
+            close(fds2[1]);
+            read(fds2[0],buf,sizeof(string2));
+            printf("parent read %s\n", buf);
+        }
+        exit(0);
+    
+    }
+    else {
+        for (i=0; i<3; i++) {
+            //write child message into fds2 close(fds2[0]);
+            write(fds2[1], string2, sizeof(string2));
+            //read parent message from fds1 close(fds1[1]);
+            read(fds1[0], buf, sizeof(string1));
+            printf("child read %s\n", buf);
+        }
+    }
 }
+
---
+++ pipe2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pipe2
+//
+//  Created by WillJia on 2018-09-23.
+//  Copyright © 2018 WillJia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
