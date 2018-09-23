c61dca76dbf23a21203931b7be021463a2b47054
Eason Jia <will.jia.sheng@gmail.com>
2018-09-23 20:14:12 -0300

    Initial Commit

---
+++ Pipe/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Pipe
+//
+//  Created by WillJia on 2018-09-23.
+//  Copyright © 2018 WillJia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
