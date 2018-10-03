7e77e124a828896a5be88cfc982d02835c5b1157
Eason Jia <will.jia.sheng@gmail.com>
2018-10-02 22:28:19 -0300

    Add function of generate input_files. Therefore, every time run this program. It will rewrite input files at first.With 25 sorted numbers in 5 files.

--- testDemo/main.cpp
+++ testDemo/main.cpp
@@ -19,6 +19,9 @@
 
 
 #include <iostream>
+#include <algorithm>    // std::sort
+
+#include <fstream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -77,6 +80,68 @@ int n = 5;
  * index = id * 2 - 1
  */
 
+/************************************************************************
+ 
+ Function:        generateInputFiles
+ 
+ Description:     sleep 10 ms
+ 
+ *************************************************************************/
+void generateInputFiles(){
+    printf("Genrate new 5 input_* files\n");
+    
+    srand((int)time(NULL));
+    int index = 0 , count = 0;
+    vector<int> array;
+    
+    int numbers[100];
+    for (int i = 1; i < 101; i++) {
+        numbers[i] = i;
+    }
+    
+    while (true) {
+        index = rand() % 100 + 1;
+        if (numbers[index] != 0) {
+            array.push_back(numbers[index]);
+            numbers[index] = 0;
+            count++;
+        }
+        if (count > 25) {
+            break;
+        }
+    }
+   
+    sort(array.begin(), array.end());
+    
+    string values[5];
+    int stringIndex = 0;
+    
+    for (int i = 1; i < 26 ;i++){
+    
+        values[stringIndex].append(to_string(array[i - 1]));
+        values[stringIndex].append(" ");
+        if (i % 5 == 0) {
+            stringIndex++;
+        }
+
+    }
+    
+    for (int i = 0; i < 5; i++) {
+        printf("%s \n" , values[i].c_str());
+    }
+    
+    string path = "/Users/WillJia/Documents/Pipe/median/input_";
+    for(int i = 1 ; i < 5 ; i++){
+        ofstream myfile;
+        myfile.open(path+to_string(i+1) + ".txt");
+        myfile << values[i];
+        myfile.close();
+    
+    }
+    
+    printf("\n");
+    
+}
 /************************************************************************
  
  Function:        sleep_gap
@@ -323,6 +388,9 @@ void childExecProgram(const char* filename){
 
 
 int main(int argc, const char * argv[]) {
+    
+    // Generate 25 random numbers into 5 input_file
+    generateInputFiles();
     
     printf("Process communication program begin.\n");
     
